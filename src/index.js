5fd516eefa89533f861e8b8ada85395d21582041
jamescross <crossjames813@gmail.com>
2021-08-10 22:38:14 -0500

    commit tree a4134005c0eeadc4a974d136ddca7a435d5a8236

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+document.addEventListener("DOMContentLoaded", function() {
+    fetchImgs();
+    fetchBreeds()
+  })
+
+  function fetchImgs(){
+    fetch(imgURL)
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+  }
+
+
+ 
