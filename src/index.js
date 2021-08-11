3209c41e6e4e8a8299657b570b6c5c32fdd07a66
jamescross <crossjames813@gmail.com>
2021-08-10 22:50:26 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+document.addEventListener("DOMContentLoaded", function() {
+    fetchImgs();
+    // fetchBreeds()
+  })
+
+  function fetchImgs(){
+    fetch(imgUrl)
+    .then(response => response.json())
+    .then(data => {
+      data.message.forEach(img => addImg(img))
+    });
+
+  }
+
+function addImg(img){
+  const container = document.getElementById('dog-image-container');
+  const newImg = document.createElement('img');
+  newImg.src = img;
+  container.appendChild(newImg);
+}
+ 
